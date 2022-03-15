f92d4333a083b21248b8c95fbe1e7b0c17fdfd19
Vyshnavick <99643242+Vyshnavick@users.noreply.github.com>
2022-03-15 11:07:54 +0400

    Update component.js

--- templates/component/component.js
+++ templates/component/component.js
@@ -1,10 +1,3 @@
-/*
-Author: Eli Elad Elrom
-Website: https://EliElrom.com
-License: MIT License
-Component: TemplateName.tsx
-*/
-
 import React from 'react';
 import './TemplateName.scss';
 import { RouteComponentProps } from 'react-router-dom'
